bb3e09a6ec6b10ce5d15a05e89528b77962d7642
Isabel Curado España <marisabelcurado.es@gmail.com>
2026-01-28 19:05:55 +0100

    Configurar API y conexión a base de datos con PDO.

--- api/config.php
+++ api/config.php
@@ -1,4 +1,7 @@
 <?php
+ini_set ('display_errors', 1);
+ini_set ('display_startup_errors', 1);
+error_reporting (E_ALL);
 
 // Configuración de cabeceras.
 header ("Access-Control-Allow-Origin: *");
@@ -34,7 +37,6 @@ try {
     ]);
     exit;
 }
-
 
 
 
